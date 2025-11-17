f08cd96e8612e61566fc67466203830ad669e1c0
keshav-const <keshavladdha005@gmail.com>
2025-11-17 21:01:48 +0530

    refactor(auth): simplify useAuth hook by integrating useShallow for state selection

--- frontend/src/hooks/use-auth.ts
+++ frontend/src/hooks/use-auth.ts
@@ -1,17 +1,15 @@
 'use client'
 
-import { shallow } from 'zustand/shallow'
-
 import { useAuthStore } from '@/store/auth'
+import { useShallow } from 'zustand/react/shallow'
 
 export const useAuth = () =>
-  useAuthStore(
-    (state) => ({
-      user: state.user,
-      token: state.accessToken,
-      isAuthenticated: state.isAuthenticated,
-      isLoading: state.isLoading,
-      signOut: state.logout,
-    }),
-    shallow
-  )
+useAuthStore(
+useShallow((state) => ({
+user: state.user,
+token: state.accessToken,
+isAuthenticated: state.isAuthenticated,
+isLoading: state.isLoading,
+signOut: state.logout,
+}))
+)
